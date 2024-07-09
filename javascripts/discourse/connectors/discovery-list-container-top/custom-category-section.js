5ed64eeffd00eb1aa45f27c8fb1b6e9f0b6a4cfe
Nikola <nikola@giveologie.com>
2024-07-09 18:28:29 +0200

    edited plugin outlet

--- javascripts/discourse/connectors/discovery-list-container-top/custom-category-section.js
+++ javascripts/discourse/connectors/discovery-list-container-top/custom-category-section.js
@@ -3,7 +3,12 @@ import { service } from "@ember/service";
 
 export default class CustomBannersComponent extends Component {
     @service currentUser;
-  
+    
+    get currentUserGroups(){
+       console.log(this.args); 
+       return  this.currentUser?.groups.map( i => i.id);
+    }
+
     get categoryBanners() {
         return settings.category_banners;
     }
