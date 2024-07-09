1a6c7be2b8cb0b697af70f747bb58f12d33fbaaf
Nikola <nikola@giveologie.com>
2024-07-09 19:39:10 +0200

    added outlet args

--- javascripts/discourse/connectors/discovery-list-container-top/custom-category-section.js
+++ javascripts/discourse/connectors/discovery-list-container-top/custom-category-section.js
@@ -11,12 +11,13 @@ export default class CustomBannersComponent extends Component {
     }
     
     get relevantCategoryBanners(){
-        const category = this.args.outletArgs.category;
+        const category = this.args.outletArgs?.category;
         if(!category){
             return [];
         }
         const subcategories = category.subcategories;
         const subcategoryIds = subcategories.map(subCat => subCat.id);
+        
         const userMemberships = this.currentUser?.groups.map( i => i.id);
         const allBanners = settings.category_banners;
         if(userMemberships.some(membership => subcategoryIds.includes(membership))){
