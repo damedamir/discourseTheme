fb4cf043789dc1b70d034cc5f13785eda80a3c11
Nikola <nikola@giveologie.com>
2024-07-09 17:11:20 +0200

    added outlet args

---
+++ javascripts/discourse/connectors/above-discovery-categories/custom-category-section.js
@@ -0,0 +1,7 @@
+import Component from "@glimmer/component";
+
+export default class CustomBannersComponent extends Component {
+  get categoryBanners() {
+    return JSON.parse(settings.banner_links);
+  }
+}
