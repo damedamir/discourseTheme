caf0f45eb44be45354829a420f39c1dac5c8c8f0
Nikola <nikola@giveologie.com>
2024-07-09 17:14:53 +0200

    edited plugin outlet

--- javascripts/discourse/connectors/above-discovery-categories/custom-category-section.js
+++ javascripts/discourse/connectors/above-discovery-categories/custom-category-section.js
@@ -2,6 +2,6 @@ import Component from "@glimmer/component";
 
 export default class CustomBannersComponent extends Component {
   get categoryBanners() {
-    return JSON.parse(settings.categoryBanners);
+    return JSON.parse(settings.category_banners);
   }
 }
