259a82e34397639d10f7b7c8d7af6ec0359487f2
Nikola <nikola@giveologie.com>
2024-07-11 18:42:15 +0200

    edited plugin outlet

--- javascripts/discourse/connectors/discovery-above/custom-category-section.js
+++ javascripts/discourse/connectors/discovery-above/custom-category-section.js
@@ -19,18 +19,27 @@ export default class CustomBannersComponent extends Component {
         if(!category){
             return [];
         }
+
         const subcategories = category.subcategories;
         const subcategoryIds = subcategories.map(subCat => subCat.id);
         
         const userMemberships = this.currentUser?.groups.map( i => i.id);
         const allBanners = settings.category_banners;
         
+        const mockSubgroupBanners = allBanners.filter( banner => {
+            if(!banner.banner_replaces_subcategory){
+                return false;
+            }
+            return !subcategoryIds.includes(banner?.subcategory_to_replace[0]);
+        } );
+
+        console.log(mockSubgroupBanners);
        /* if(subcategories.length > 0){
             return [];
         }
          */
       
-        const relevantBanners = allBanners.filter( banner => banner.group.includes(category.id));
+        const relevantBanners = allBanners.filter( banner => banner.group.includes(category.id) );
 
         return relevantBanners;
 
