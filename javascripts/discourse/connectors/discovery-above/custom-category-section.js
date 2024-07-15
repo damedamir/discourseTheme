35f92aa113bf51e1c09406f0298500806569704c
Nikola <nikola@giveologie.com>
2024-07-15 22:27:56 +0200

    added Subcategory banners

--- javascripts/discourse/connectors/discovery-above/custom-category-section.js
+++ javascripts/discourse/connectors/discovery-above/custom-category-section.js
@@ -22,11 +22,10 @@ export default class CustomBannersComponent extends Component {
                     'Api-Key' : 'c8a73fd76bd70c08ee2b9184f6ed89a8e0daa3a4c9a867a75545d232272ed997',
                     'Api-Username' : 'System'
                 }
-            }).then( res => res.json()).then(data => { this.subcategories_with_positions = data.category_list; console.log(this.subcategories_with_positions) });
+            }).then( res => res.json()).then(data => { this.subcategories_with_positions = data.category_list;});
         }catch (error) {
             console.log('Failed:' , error);
         }
-        return this.subcategories_with_positions;
 
     }
     
