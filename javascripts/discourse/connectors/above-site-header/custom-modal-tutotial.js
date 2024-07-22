9a4bdad0b313a272904453a9af63fc98eac6f1e6
Nikola <nikola@giveologie.com>
2024-07-22 19:46:07 +0200

    added Subcategory banners

---
+++ javascripts/discourse/connectors/above-site-header/custom-modal-tutotial.js
@@ -0,0 +1,12 @@
+import Component from "@glimmer/component";
+import { service } from "@ember/service";
+
+export default class IntroModal extends Component {
+    @service currentUser;
+
+    get userIsNew(){
+        return !this.currentUser.isNew;
+    }
+
+
+}
