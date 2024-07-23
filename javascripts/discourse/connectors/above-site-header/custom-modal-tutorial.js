c3516677313bba1990d5a43c29665e0497d02bd0
Nikola <nikola@giveologie.com>
2024-07-23 19:33:11 +0200

    added isnew Selector

--- javascripts/discourse/connectors/above-site-header/custom-modal-tutorial.js
+++ javascripts/discourse/connectors/above-site-header/custom-modal-tutorial.js
@@ -2,14 +2,16 @@ import Component from "@glimmer/component";
 import { service } from "@ember/service";
 import {CustomModalTutorial} from "./custom-modal-tutorial";
 import { action } from "@ember/object";
+import { tracked } from "@glimmer/tracking";
 
 export default class IntroModal extends Component {
     @service currentUser;
     @service modal;
     @service router;
+    
 
-    modalIsVisible = true;
-    alreadyViewed = false;
+    @tracked modalIsVisible = true;
+    @tracked alreadyViewed = false;
 
 
     closeModal(){
