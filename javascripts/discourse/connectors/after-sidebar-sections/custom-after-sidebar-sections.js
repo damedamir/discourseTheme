f44287d57c46c1cfbd45a182c1a77541e2729bcf
Nikola <nikola@giveologie.com>
2025-02-28 20:10:58 +0100

    added custom CTA button wrap

--- javascripts/discourse/connectors/after-sidebar-sections/custom-after-sidebar-sections.js
+++ javascripts/discourse/connectors/after-sidebar-sections/custom-after-sidebar-sections.js
@@ -15,6 +15,29 @@ export default class CustomBannersComponent extends Component {
             const categoriesContainer = document.getElementById('sidebar-section-content-categories');
             if(categoriesContainer){
                 const parser = new DOMParser()
+                const newMicroTacticsNode = parser.parseFromString(
+                    `
+                <li class="sidebar-section-link-wrapper" >
+          <a  class="ember-view sidebar-section-link sidebar-row" target="_blank" href="https://www.hiveologie.com/resh/a/en/category/post/287">
+            
+      <span style="color: #31a300" class="sidebar-section-link-prefix span">
+          <span style="background: linear-gradient(90deg, #31a300 50%, #31a300 50%)" class="prefix-span"></span>
+        
+         
+      </span>
+  
+
+            <span class="sidebar-section-link-content-text">
+              Micro Tactics
+              <!---->
+            </span>
+
+<!---->
+<!---->
+<!---->          </a>
+      </li>
+                `, 'text/html'
+                )
                const newResourcesNode = parser.parseFromString(`
                 <li class="sidebar-section-link-wrapper" >
           <a  class="ember-view sidebar-section-link sidebar-row" target="_blank" href="https://www.hiveologie.com/resh/a/en/subscription/resources/4849/all-access-pass">
