ab3cf9a62a9e8da3187940fbc2235ca6ff13c33f
Nikola <nikola@giveologie.com>
2025-03-27 15:24:29 +0100

    Added group and tag theme setting

---
+++ javascripts/discourse/connectors/topic-list-item/topic-list-item.js
@@ -0,0 +1,27 @@
+import Component from "@glimmer/component";
+import { service } from "@ember/service";
+import { tracked } from "@glimmer/tracking";
+import { action } from "@ember/object";
+
+import {ajax} from "discourse/lib/ajax";
+
+export default class HiddenMicroTactic extends Component {
+    @service currentUser;
+    @service siteSettings;
+    @service router;
+
+    constructor() {
+        super(...arguments);   
+        console.log(this.args.outletArgs);
+      }
+
+   get isAdminPage(){
+        console.log(this.args.outletArgs.currentPath);
+        return this.args.outletArgs.currentPath.split('.')[0] === 'admin' && !(this.args.outletArgs.currentPath.split('.')[1] === 'dashboard') ;
+   } 
+
+   @action
+   navigateToDashboard(){
+    this.router.transitionTo("admin.dashboard.general");
+   }
+}
