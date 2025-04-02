34ef75b182cd63b384bfa96d4b10febb94443d59
Nikola <nikola@giveologie.com>
2025-04-02 20:23:52 +0200

    Added desctiprions and labels for MT restriction settings

---
+++ javascripts/discourse/connectors/topic-above-post-stream/topic-above-post-stream.js
@@ -0,0 +1,29 @@
+import Component from "@glimmer/component";
+import { service } from "@ember/service";
+import { tracked } from "@glimmer/tracking";
+import { action } from "@ember/object";
+
+import {ajax} from "discourse/lib/ajax";
+
+export default class DirectLinRestriction extends Component {
+    @service currentUser;
+    @service siteSettings;
+    @service router;
+
+    constructor() {
+        super(...arguments);   
+        console.log("Above post stream Outlet Args");
+        console.log(this.args.outletArgs);
+        /*console.log(settings.micro_tactic_restristions);*/
+      }
+
+   get isAdminPage(){
+        console.log(this.args.outletArgs.currentPath);
+        return this.args.outletArgs.currentPath.split('.')[0] === 'admin' && !(this.args.outletArgs.currentPath.split('.')[1] === 'dashboard') ;
+   } 
+
+   @action
+   navigateToDashboard(){
+    this.router.transitionTo("admin.dashboard.general");
+   }
+}
