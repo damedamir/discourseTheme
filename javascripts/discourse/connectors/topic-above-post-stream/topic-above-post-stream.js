09c60379304972fe13648165d8d0567cab3108e4
Nikola <nikola@giveologie.com>
2025-04-11 19:38:47 +0200

    Testing MC on page visit integration

--- javascripts/discourse/connectors/topic-above-post-stream/topic-above-post-stream.js
+++ javascripts/discourse/connectors/topic-above-post-stream/topic-above-post-stream.js
@@ -44,15 +44,21 @@ export default class DirectLinRestriction extends Component {
           }
 
         /*console.log(settings.micro_tactic_restristions);*/
-        console.log(this.currentUser);
-        const downloadButon = document.querySelector('.attachment.pdf-attachment');
-        console.log('Button Element on load');
-        console.log(downloadButon);
-        downloadButon?.addEventListener('click', function(e){
-          console.log('clicked PDF button');
-          console.log(e);
-        });
+       
       }
+    
+   didInsertElement(){
+    this._super(...arguments);
+    
+    console.log(this.currentUser);
+    const downloadButon = document.querySelector('.attachment.pdf-attachment');
+    console.log('Button Element on load');
+    console.log(downloadButon);
+    downloadButon?.addEventListener('click', function(e){
+      console.log('clicked PDF button');
+      console.log(e);
+    });
+   }   
 
 
    @action
